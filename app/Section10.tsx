a308ed28e250e8c415c1a0221a9a067bbd82e5eb
HakimGolshahi <golshahy@gmail.com>
2025-05-24 09:54:17 +0330

    init222

--- app/Section10.tsx
+++ app/Section10.tsx
@@ -4,6 +4,8 @@ const Section10 = () => {
   return (
     <div className="flex w-full justify-center items-center">
       <div className="">
+
+        
         <h1>اسلایدر با یک قابل مشاهده</h1>
         <InfiniteSlider />
       </div>
